75c4bf638d1605c1c2434a14ef1bfb7e0d63c181
galaxy4276 <galaxyhi4276@gmail.com>
2020-11-28 21:42:01 +0900

    feat: indexController 와 mustache 페이지 연동

--- src/main/java/com/galaxy4276spring/Application.java
+++ src/main/java/com/galaxy4276spring/Application.java
@@ -8,7 +8,9 @@ package com.galaxy4276spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
