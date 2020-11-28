20c2210ae6edb85042d8bf77f88186584655d159
galaxy4276 <galaxyhi4276@gmail.com>
2020-11-28 23:27:28 +0900

    feat: 게시글 조회 및 Model 불러오기

--- src/main/java/com/galaxy4276spring/domain/posts/PostsRepository.java
+++ src/main/java/com/galaxy4276spring/domain/posts/PostsRepository.java
@@ -1,11 +1,15 @@
 package com.galaxy4276spring.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 /*
   JpaRepository<Entity Class, PK Type> 상속 시 기본적 CRUD 메서드 자동 생성
  */
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
+  @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+  List<Posts> findAllDesc();
 }
 
--- src/main/java/com/galaxy4276spring/service/posts/PostsService.java
+++ src/main/java/com/galaxy4276spring/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.galaxy4276spring.service.posts;
 
 import com.galaxy4276spring.domain.posts.Posts;
 import com.galaxy4276spring.domain.posts.PostsRepository;
+import com.galaxy4276spring.web.dto.PostsListResponseDto;
 import com.galaxy4276spring.web.dto.PostsResponseDto;
 import com.galaxy4276spring.web.dto.PostsSaveRequestDto;
 import com.galaxy4276spring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import javax.transaction.Transactional;
+
+import java.util.stream.Collectors;
+import java.util.List;
+
 
 @RequiredArgsConstructor
 @Service
@@ -30,10 +34,18 @@ public class PostsService {
     return id;
   }
 
+  @Transactional
   public PostsResponseDto findById (Long id) {
     Posts entity = postsRepository.findById(id).orElseThrow(() -> new
       IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
     return new PostsResponseDto(entity);
+  }
+
+  @Transactional
+  public List<PostsListResponseDto> findAllDesc() {
+    return postsRepository.findAllDesc().stream()
+        .map(PostsListResponseDto::new)
+        .collect(Collectors.toList());
   }
 }
